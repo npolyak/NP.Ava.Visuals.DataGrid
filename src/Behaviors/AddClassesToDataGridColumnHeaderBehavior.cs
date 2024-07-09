4e3fe7804121de8336bcd9f097ad53d3aba357bc
npolyak <nick.polyak@verizon.net>
2024-07-08 21:57:38 -0400

    moved part of the functionality over to NP.Ava.Visuals.DG

---
+++ src/Behaviors/AddClassesToDataGridColumnHeaderBehavior.cs
@@ -0,0 +1,80 @@
+﻿using NP.Concepts.Behaviors;
+using System.Collections.Generic;
+using Avalonia.Controls;
+using Avalonia;
+using System;
+using NP.Utilities;
+
+namespace NP.Ava.Visuals.DG.Behaviors
+{
+    public static class AddClassesToDataGridColumnHeaderBehavior
+    {
+        #region TheClassesToAdd Attached Avalonia Property
+        public static string GetTheClassesToAdd(DataGrid obj)
+        {
+            return obj.GetValue(TheClassesToAddProperty);
+        }
+
+        public static void SetTheClassesToAdd(DataGrid obj, string value)
+        {
+            obj.SetValue(TheClassesToAddProperty, value);
+        }
+
+        public static readonly AttachedProperty<string> TheClassesToAddProperty =
+            AvaloniaProperty.RegisterAttached<DataGrid, DataGrid, string>
+            (
+                "TheClassesToAdd"
+            );
+        #endregion TheClassesToAdd Attached Avalonia Property
+
+        static AddClassesToDataGridColumnHeaderBehavior()
+        {
+            TheClassesToAddProperty.Changed.Subscribe(OnClassesToAddChanged);
+        }
+
+
+        #region TheBehavior Attached Avalonia Property
+        private static BehaviorsDisposable<IEnumerable<DataGridColumn>> GetTheBehavior(DataGrid obj)
+        {
+            return obj.GetValue(TheBehaviorProperty);
+        }
+
+        private static void SetTheBehavior(DataGrid obj, BehaviorsDisposable<IEnumerable<DataGridColumn>> value)
+        {
+            obj.SetValue(TheBehaviorProperty, value);
+        }
+
+        private static readonly AttachedProperty<BehaviorsDisposable<IEnumerable<DataGridColumn>>> TheBehaviorProperty =
+            AvaloniaProperty.RegisterAttached<DataGrid, DataGrid, BehaviorsDisposable<IEnumerable<DataGridColumn>>>
+            (
+                "TheBehavior"
+            );
+        #endregion TheBehavior Attached Avalonia Property
+
+
+        private static void OnClassesToAddChanged(AvaloniaPropertyChangedEventArgs<string> args)
+        {
+            DataGrid dataGrid = (DataGrid)args.Sender;
+
+            SetTheBehavior(dataGrid, dataGrid.Columns.AddBehavior(OnColumnAdded));
+        }
+
+        private static void OnColumnAdded(DataGridColumn col)
+        {
+            DataGrid dataGrid = col.GetPropValue<DataGrid>("OwningGrid", true);
+            DataGridColumnHeader header = col.GetPropValue<DataGridColumnHeader>("HeaderCell", true);
+
+            string classesToAdd = GetTheClassesToAdd(dataGrid);
+
+            if (!string.IsNullOrEmpty(classesToAdd))
+            {
+                header.Classes.AddRange(classesToAdd.GetClasses());
+            }
+        }
+
+        internal static string[] GetClasses(this string classesStr)
+        {
+            return classesStr.Split(StrUtils.WHITESPACE_CHARS, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ src/Behaviors/ColumnSerializationData.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace NP.Ava.Visuals.DG.Behaviors
+{
+    public class ColumnSerializationData
+    {
+        [XmlAttribute]
+        public string? HeaderId { get; set; }
+
+        [XmlAttribute]
+        public bool IsVisible { get; set; }
+
+        [XmlAttribute]
+        public string? WidthStr { get; set; }
+    }
+}
